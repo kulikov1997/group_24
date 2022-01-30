ff329faa216341d3b88e904bc1f30ebc85140957
kulikov1997 <93608590+kulikov1997@users.noreply.github.com>
2022-01-30 20:31:29 +0300

    Update LESSON_3.py

--- PYTHON/LESSON_3.py
+++ PYTHON/LESSON_3.py
@@ -50,11 +50,11 @@ if div_int < comp_item:
 
 # 23. Создать переменную item_1 в которй прибавить 10 к переменной int_item
 
-item_1 = 10 + int_item
+item_1 = int_item + 10
 
 # 24. Сделать if в котором будет условие: если item_1 меньше comp_item, то вывести в консоль (“Переменная item_1 меньше”, comp_item), иначе, вывести в консоль (“Переменная item_1 больше или равна”, comp_item)
 
-if item_1 < comp_item:
+if item_1 >= comp_item:
     print("Переменная item_1 меньше <", comp_item)
 else:
     print("Переменная item_1 больше или равна >=", comp_item)
@@ -64,28 +64,28 @@ else:
 if item_2:
     print("Переменная item_2 =", item_2)
 else:
-    print("Переменная item_2 =", item_3)
+    print("Переменная item_2 = ", item_3)
 
 # 26. Сделать if в котором будет условие: если item_3, то вывести в консоль (“Переменная item_3 = ”, item_2), иначе, вывести в консоль (“Переменная item_3 = ”, item_3)
 
 if item_3:
-    print("Переменная item_3 =", item_2)
+    print("Переменная item_3 = ", item_2)
 else:
-    print("Переменная item_3 =", item_3)
+    print("Переменная item_3 = ", item_3)
 
 # 27. Сделать if в котором будет условие: если item_5, то вывести в консоль (“Переменная item_5 = ”, item_5), иначе, вывести в консоль (“Переменная item_5 = ”, item_4)
 
 if item_5:
     print("Переменная item_5 =", item_5)
 else:
-    print("Переменная item_5 =", item_4)
+    print("Переменная item_5 = ", item_4)
 
 # 28. Сделать if в котором будет условие: если item_4, то вывести в консоль (“Переменная item_4 = ”, item_5), иначе, вывести в консоль (“Переменная item_4 = ”, item_4)
 
 if item_4:
     print("Переменная item_4 =", item_5)
 else:
-    print("Переменная item_4 =", item_4)
+    print("Переменная item_4 = ", item_4)
 
 # 29. Создать переменную currency_convertor со значением item_2
 currency_convertor = item_2
@@ -105,22 +105,32 @@ if currency_convertor:
     target_currency = eur_item
     target_currency_amount = 50
     currency_result = 0
-if target_currency == "eur":
-    currency_result = target_currency_amount / usd_eur_rate
-    print(target_currency_amount, eur_item, "=", currency_result, usd_item)
-elif target_currency == "uah":
-    currency_result = target_currency_amount / usd_uah_rate
-    print(target_currency_amount, uah_item, "=", currency_result, usd_item)
-elif target_currency == "chf":
-    currency_result = target_currency_amount / usd_chf_rate
-    print(target_currency_amount, chf_item, "=", currency_result, usd_item)
-elif target_currency == "rub":
-     currency_result = target_currency_amount / usd_rub_rate
-     print(target_currency_amount, rub_item, "=", currency_result, usd_item)
-elif target_currency == "byn":
-    currency_result = target_currency_amount / usd_byn_rate
-    print(target_currency_amount, byn_item, "=", currency_result, usd_item)
 
+    if target_currency == "eur":
+      currency_result = target_currency_amount / usd_eur_rate
+      print(target_currency_amount, eur_item, "=", currency_result, usd_item)
 
+    elif target_currency == "uah":
+      currency_result = target_currency_amount / usd_uah_rate
+      print(target_currency_amount, uah_item, '=', currency_result, usd_item)
+
+    elif target_currency == "eur":
+      currency_result = target_currency_amount / usd_eur_rate
+      print(target_currency_amount, eur_item, "=", currency_result, usd_item)
+
+    elif target_currency == "chf":
+      currency_result = target_currency_amount / usd_chf_rate
+      print(target_currency_amount, chf_item, "=", currency_result, usd_item)
+
+    elif target_currency == "rub":
+      currency_result = target_currency_amount / usd_rub_rate
+      print(target_currency_amount, rub_item, "=", currency_result, usd_item)
+
+    elif target_currency == "byn":
+      currency_result = target_currency_amount / usd_byn_rate
+      print(target_currency_amount, "=", byn_item, currency_result, usd_item)
+
+    else:
+      print("Unknow currency")
 else:
-    print("Unknown currency", target_currency)
+    print("Переменная currency_convertor =", item_3)
